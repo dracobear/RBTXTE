93f9a312f9372805785f6c84284dfcca41161afd
draco <145726069+dracobear@users.noreply.github.com>
2023-09-23 13:08:47 -0400

    version two
    has an issue, i need to fix it

--- RBTXTE.py
+++ RBTXTE.py
@@ -1,8 +1,10 @@
 from colorama import Fore
-version = "v0.01 (doukutsu)"
+import os
+version = "v0.02 (monogatari)"
 print("RBTXTE "+version)
 print("allways remember to end your filenames in .txt!")
-print("new = make new file, open = open file, edit = edit file")
+helpmessage = "new (filename). makes a new file \nopen (filename) reads a file, \nedit (filename) edits a file \nrename (filename) renames a file \nhelp shows this message \nexit quits the program"
+print(helpmessage)
 while True:
     put = input("what do you want to do? ")
     if put.startswith("open ") == True:
@@ -19,10 +21,10 @@ while True:
         try:
             writ = input("add or overwrite? ")
             if writ == "add":
-                wrot == input("text to append? ")
+                wrot = input("text to append? ")
                 open(aput, 'a').write(wrot)
             elif writ == "overwrite":
-                wrot == input("new text? ")
+                wrot = input("new text? ")
                 open(aput, 'w').write(wrot)
             else:
                 ("thats not an option supported by the editor")
@@ -38,3 +40,18 @@ while True:
             print(Fore.RED + aput + ": Error, file allready exsists" + Fore.WHITE)
         except PermissionError:
             print(Fore.RED + aput + ": Error, access denied" + Fore.WHITE)
+    elif put.startswith("rename ") == True:
+        aput = put.replace("rename ", "")
+        try:
+            newname = input("new file name? ")
+            os.rename(aput, newname)
+        except FileNotFoundError:
+            print(Fore.RED + aput + ": Error, file not found" + Fore.WHITE)
+        except PermissionError:
+            print(Fore.RED + aput + ": Error, access denied" + Fore.WHITE)
+        except  FileExistsError:
+            print(Fore.RED + aput + ": Error, filename taken" + Fore.WHITE)
+    elif put == "help":
+        print(helpmessage)
+    elif put == "exit":
+        exit()
